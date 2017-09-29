4f85ed762e5101e5997c0d1a6279c94a539515af
jungaboo <fredriklarsenmail@gmail.com>
2017-09-29 14:29:45 +0200

    gulp struggle
    gulp struggle

---
+++ app/js/main.js
@@ -0,0 +1,1 @@
+var maijs = 1;
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
 
-gulp.task('sass', function(){
-	return gulp.src('app/scss/style.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('app/css'))
+gulp.task('stream', function () {
+	// Endless stream mode
+    return watch('css/style.css', { ignoreInitial: false })
+        .pipe(gulp.dest('build'));
 });
+
+gulp.task('callback', function () {
+	// Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('css/style.css', function () {
+        gulp.src('css/style.css')
+            .pipe(gulp.dest('build'));
+    });
+});
+
+gulp.task('default', ['stream','callback']);
